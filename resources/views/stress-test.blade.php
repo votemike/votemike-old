740b95d0485c050fdeadc2d43e3b86b8b122eba7
Michael Gwynne <michael@votemike.co.uk>
2017-01-27 19:30:02 +0000

    Adding more interest rates to stress test

--- resources/views/stress-test.blade.php
+++ resources/views/stress-test.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Property Stress Test Tool')
 
 @section('stylesheets')
-    <link rel="stylesheet" href="{{asset('css/styles.0a2b245ed7374ac7c2aa.bundle.css')}}">
+    <link rel="stylesheet" href="{{asset('css/styles.67cb29349f87f2fc9d83.bundle.css')}}">
 @endsection
 
 @section('scripts')
-    <script type="text/javascript" src="{{asset('js/inline.ef3229396d0e97944edc.bundle.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/inline.7896741c4028ef096911.bundle.js')}}"></script>
     <script type="text/javascript" src="{{asset('js/vendor.bc38d347f6accd101012.bundle.js')}}"></script>
-    <script type="text/javascript" src="{{asset('js/main.0b8e6a9d90673843a55a.bundle.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/main.82402e0f274aa9172897.bundle.js')}}"></script>
 @endsection
 
 @section('content')
