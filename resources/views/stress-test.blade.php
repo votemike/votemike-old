005818b3c7518c09b523230fdf5105abd021856c
Michael Gwynne <michael@votemike.co.uk>
2017-02-12 16:43:30 +0000

    Adding income to stress test

--- resources/views/stress-test.blade.php
+++ resources/views/stress-test.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Property Stress Test Tool')
 
 @section('stylesheets')
-    <link rel="stylesheet" href="{{asset('css/styles.67cb29349f87f2fc9d83.bundle.css')}}">
+    <link rel="stylesheet" href="{{asset('css/styles.baa631c31e7e3f898892.bundle.css')}}">
 @endsection
 
 @section('scripts')
-    <script type="text/javascript" src="{{asset('js/inline.7896741c4028ef096911.bundle.js')}}"></script>
-    <script type="text/javascript" src="{{asset('js/vendor.bc38d347f6accd101012.bundle.js')}}"></script>
-    <script type="text/javascript" src="{{asset('js/main.82402e0f274aa9172897.bundle.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/inline.113f495639ffe71cb038.bundle.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/vendor.293bb1a227b08dd51e21.bundle.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/main.3c60a92e1c1ab4d4f188.bundle.js')}}"></script>
 @endsection
 
 @section('content')
